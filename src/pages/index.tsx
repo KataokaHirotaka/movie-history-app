dfdf4c52be3f45a0963f12a5229f43b824a9275b
HirotakaKataoka <h-kataoka@bfp.co.jp>
2022-10-06 16:36:58 +0900

    メインページの実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import type { NextPage } from 'next'
+import styled from 'styled-components'
 import { HomeLayout } from '../components/layouts/HomeLayout'
 
 const Home: NextPage = () => {
   return (
     <HomeLayout>
-      <h2>test</h2>
-      <a href="">faa</a>
+      <Main>
+        <h2>test</h2>
+        <a href="">faa</a>
+      </Main>
     </HomeLayout>
   )
 }
 
 export default Home
+
+
+/**
+ * スタイル定義
+ */
+
+const Main = styled.main`
+  ${({ theme }) => theme.breakpoint.base`
+    // padding: 47px ${theme.space[2]} 0;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding-top: 47px;
+  `}
+`
