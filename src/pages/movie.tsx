053abf8f75e345a4672e8319656f64f8e945a35e
片岡潤隆 <>
2022-10-11 10:24:34 +0900

    各ページの実装

---
+++ src/pages/movie.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PageLayout } from '../components/layouts/PageLayout'
+
+const Movie = () => {
+  return (
+    <PageLayout title="映画 - MovieHistory" description="映画情報">
+      <p>test</p>
+    </PageLayout>
+  )
+}
+
+export default Movie
