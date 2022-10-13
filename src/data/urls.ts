e94057451c368d506d242ba6981791cf2f95027c
HirotakaKataoka <h-kataoka@bfp.co.jp>
2022-10-13 20:35:08 +0900

    url変更

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -1,5 +1,5 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY
 
 export const urls = {
-  popularMovie: `https://api.themoviedb.org/3/movie/550?api_key=0965bb0b2ed4a6b448e51c23934b8931`,
+  popularMovie: `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`,
 }
